2918dec0dbff62701d1f17c225d6cf240d64d8fd
kunal Dugar <yoyokvunal@gmail.com>
2024-07-02 09:52:47 +0530

    footer added
    Signed-off-by: kunal Dugar <yoyokvunal@gmail.com>

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 //import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import HeroSection from "./components/HeroSection";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <div className="hero-section-background">
       <Navbar />
       <HeroSection />
+      <Footer />
+
     </div>
   );
 }
