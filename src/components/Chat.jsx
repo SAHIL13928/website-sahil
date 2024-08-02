e2722d1a1c283f6d8eb7e572dda1bd6f37b535a6
kunal Dugar <yoyokvunal@gmail.com>
2024-08-02 10:36:40 +0530

    Change some styles of chat component

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -1,12 +1,17 @@
 import { useState } from 'react';
 import { BsChatRightDots } from "react-icons/bs";
 import { AiOutlineClose } from "react-icons/ai";
-import { Box, IconButton, Typography, Paper, Card, CardContent } from '@mui/material';
+import { Box, IconButton, Typography, Paper, Card, CardContent, TextField } from '@mui/material';
+import { LuSend } from "react-icons/lu";
+
 
 
 
 const Chat = () => {
     const [isOpen, setIsOpen] = useState(false);
+    const [message, setMessage] = useState("");
+    const [messages, setMessages] = useState([]);
+
 
 
 
@@ -14,11 +19,18 @@ const Chat = () => {
         setIsOpen(!isOpen);
     };
 
+    const handleSendMessage = (e) => {
+        if (message.trim()) {
+            setMessages([...messages, message]);
+            setMessage("");
+        }
+
+    }
 
 
 
     return (
-        <Box position="fixed" bottom={16} right={16}>
+        <Box position="fixed" bottom={16} right={24}>
             {!isOpen && (
                 <div className='flex gap-2'>
                     <Card sx={{ border: "1px solid #EBF1F0", bgcolor: "#EBF1F0" }}>
@@ -52,7 +64,7 @@ const Chat = () => {
             )}
 
             {isOpen && (
-                <Paper elevation={0} sx={{ mt: 1, width: 356, height: 400, backgroundColor: "blue", borderRadius: "1rem", overflow: "hidden", border: "none", }}>
+                <Paper elevation={0} sx={{ mt: 1, width: 400, height: 450, backgroundColor: "blue", borderRadius: "1rem", overflow: "hidden", border: "none", mr: 2 }}>
                     <Box sx={{ position: "relative", height: "100%", overflow: "hidden" }}>
                         <IconButton
                             onClick={toggleChat}
@@ -61,6 +73,8 @@ const Chat = () => {
                                     backgroundColor: 'transparent',
                                 },
                                 position: 'absolute',
+                                display: 'flex',
+                                alignItems: 'center',
                                 top: 0,
                                 right: 0,
                                 zIndex: 1,
@@ -68,18 +82,44 @@ const Chat = () => {
                         >
                             <AiOutlineClose />
                         </IconButton>
-                        <Box sx={{ p: 3, backgroundColor: "white", width: "100%", height: "100%", display: "flex", flexDirection: "column", overflowY: "auto", mt: 4 }}>
-                            {Array.from({ length: 10 }).map((_, index) => (
-                                <Typography sx={{ overflow: "hidden" }} key={index} variant="body1" className='bg-slate-100 p-12' color="initial">
-                                    part{index + 1}
+                        <Box sx={{ p: 3, backgroundColor: "white", width: "100%", height: "100%", display: "flex", flexDirection: "column", overflowY: "auto", marginY: 6, gap: 2 }}>
+                            {/* <p>logo</p> */}
+                            <Typography variant="body1" className='bg-slate-100 py-4 px-2 rounded-r-xl rounded-b-xl   ' color="initial">
+                                How can we help you today?
+                            </Typography>
+                            {messages.map((msg, index) => (
+                                <Typography key={index} variant="body1" className='bg-slate-100 py-4 px-2 rounded-r-xl rounded-b-xl' color="initial">
+                                    {msg}
                                 </Typography>
                             ))}
+                            {/* {Array.from({ length: 20 }).map((_, index) => (
+                                <Typography key={index} variant="body1" className='bg-slate-100 py-4 px-2 rounded-r-xl rounded-b-xl   ' color="initial">
+                                    part{index + 1}
+                                </Typography>
+                            ))} */}
+
                         </Box>
+
                     </Box>
+                    <div className='mb-8 mt flex items-center gap-2 cursor-pointer  sticky bottom-0 bg-blue-700  '>
+                        <TextField
+                            className='w-[90%]'
+                            id=""
+                            placeholder="Enter your message"
+                            value={message}
+                            onChange={(e) => setMessage(e.target.value)}
+                            onKeyDown={(e) => e.key === 'Enter' && handleSendMessage()}
+
+                        />
+                        <LuSend onClick={handleSendMessage} />
+
+                    </div>
                 </Paper>
 
 
+
             )}
+
         </Box>
     );
 };
